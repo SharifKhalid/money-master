71b5184c78722fcebc21bad72ed684125280e5be
SharifKhalid <sharifkhalid161097@gmail.com>
2022-03-02 14:28:28 +0600

    beautify

--- Search-Phone.js
+++ Search-Phone.js
@@ -1,3 +1,4 @@
+    // API data load Function
 const searchPhone = () => {
     const input = document.getElementById('searchInput');
     const value = input.value;
@@ -10,13 +11,14 @@ const searchPhone = () => {
      .then(data => displyPhone(data.data));
      console.log(data);
 }
-
+    // data disply function
 const displyPhone = elements => { 
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     
     // Showing Phone is not found
     const alertshow = document.getElementById('alert-show');
+    alertshow.textContent = '';
     if(elements.length == 0){
         const alert = document.createElement('h1');
         alert.innerHTML =`
@@ -25,10 +27,14 @@ const displyPhone = elements => {
         alertshow.appendChild(alert);    
     }
 
-    
+    // if( elements.length <=20){
+    //     console.log('working');
+    // }
+    // else{
+    //     console.log('larger than 20');
+    // }
         for(const element of elements) {
-            console.log(elements.length);
-                        
+            
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -37,6 +43,8 @@ const displyPhone = elements => {
                     <div class="card-body w-75">
                     <h3 class="">${element.phone_name}</h3>
                     <h3 class="">${element.brand}</h3>
+                    <h4> Sensors </h4> 
+                    
                     <button id="details" type="button" class="btn btn-primary btn-lg"> Show Details </button>
                     </div>
                 </div>
@@ -44,3 +52,4 @@ const displyPhone = elements => {
             searchResult.appendChild(div);
         };
 }
+
