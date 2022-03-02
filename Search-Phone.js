cd2cd3cb3b7153b5db8be6f54bff2e85271cdd57
SharifKhalid <sharifkhalid161097@gmail.com>
2022-03-02 18:41:17 +0600

    submit

--- Search-Phone.js
+++ Search-Phone.js
@@ -1,4 +1,5 @@
-    // API data load Function
+   
+   // API data load Function
 const searchPhone = () => {
     const input = document.getElementById('searchInput');
     const value = input.value;
@@ -9,7 +10,7 @@ const searchPhone = () => {
     fetch(url)
      .then(res => res.json())
      .then(data => displyPhone(data.data));
-     console.log(data);
+     
 }
 
     // data disply function
@@ -30,24 +31,22 @@ const displyPhone = elements => {
 
         // Loop 
         for(const element of elements) {
-            
-            
-            // create new div
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
-                <div class="card w-75 mx-auto ">
-                    <img src="${element.image}" class="card-img-top w-50 h-50 " alt="">
-                    <div class="card-body w-75">
-                    <h3 class="">${element.phone_name}</h3>
-                    <h3 class="">${element.brand}</h3>
-                    <h4> Sensors </h4> 
-                    <button id="details" onclick="detailShow('${element.slug}')" type="button" class="btn btn-primary btn-lg"> Show Details </button>
-                    </div>
+         
+        // create new div
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card w-75 mx-auto ">
+                <img src="${element.image}" class="card-img-top w-50 h-50 " alt="">
+                <div class="card-body w-75">
+                <h3 class="">${element.phone_name}</h3>
+                <h3 class="">${element.brand}</h3> 
+                <button id="details" onclick="detailShow('${element.slug}')" type="button" class="btn btn-primary btn-lg"> Show Details </button>
                 </div>
-            `
-            searchResult.appendChild(div);
-        };
+            </div>
+        `
+        searchResult.appendChild(div);
+    };
 }
 
     // URL for detail information
